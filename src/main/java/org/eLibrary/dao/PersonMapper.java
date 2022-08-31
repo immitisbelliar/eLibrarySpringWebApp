48243e1e0df493d0f80948c9567a066f46a8bb03
Yaroslav <mr.yaroslav.g.a@gmail.com>
2022-08-31 18:19:04 +0600

    Create PersonMapper.java

---
+++ src/main/java/org/eLibrary/dao/PersonMapper.java
@@ -0,0 +1,20 @@
+package org.eLibrary.dao;
+
+import org.eLibrary.models.Person;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class PersonMapper implements RowMapper<Person> {
+    @Override
+    public Person mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Person person = new Person();
+
+        person.setId(rs.getInt("person_id"));
+        person.setName(rs.getString("name"));
+        person.setYearOfBirth(rs.getInt("yearOfBirth"));
+
+        return person;
+    }
+}
