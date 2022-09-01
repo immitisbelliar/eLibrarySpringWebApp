35fa8f177f93294706277fc256f7086dc7ab583c
Yaroslav <mr.yaroslav.g.a@gmail.com>
2022-09-01 17:05:24 +0600

    Add methods to manage books in BookDAO.java

--- src/main/java/org/eLibrary/dao/BookDAO.java
+++ src/main/java/org/eLibrary/dao/BookDAO.java
@@ -1,11 +1,13 @@
 package org.eLibrary.dao;
 
 import org.eLibrary.models.Book;
+import org.eLibrary.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class BookDAO {
@@ -38,5 +40,17 @@ public class BookDAO {
         jdbcTemplate.update("DELETE FROM Book WHERE book_id=?", id);
     }
 
+    public Optional<Person> getBookOwner(int id) {
+        return jdbcTemplate.query("SELECT Person.* FROM Book JOIN Person ON Book.person_id = Person.person_id" +
+                "WHERE Book.book_id=?", new Object[]{id}, new PersonMapper()).stream().findAny();
+    }
+
+    public void release(int id) {
+        jdbcTemplate.update("UPDATE Book SET person_id=NULL WHERE book_id=?", id);
+    }
+
+    public void assign(int id, Person selectedPerson) {
+        jdbcTemplate.update("UPDATE Book SET person_id=? WHERE book_id=?", selectedPerson.getId(), id);
+    }
 }
 
